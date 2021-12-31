67144836066c6ea45e8431769f1df895941e7ed7
Imash Nidusha Fernando <90578660+Imash-BoT@users.noreply.github.com>
2021-12-31 10:28:48 +0530

    Update reels.js

--- plugins/reels.js
+++ plugins/reels.js
@@ -17,13 +17,13 @@ Asena.addCommand({pattern: 'insta ?(.*)', fromMe: false, desc: "Downloads from i
     var reply = await message.client.sendMessage(message.jid, LOAD_ING , MessageType.text, { quoted: message.data });
     
    const { status, type, data } = await instagram(match[1], 'julie')
-    if (!status) return await message.sendMessage('✅️Example:-\n\n*Change /reel/ to /p/ Then give Command .insta*\n\n*Any doubt ask to Nijin or Husni*')
+    if (!status) return await message.sendMessage('✅️Example:-\n\n*Change /reel/ to /p/ Then give Command .insta*\n\n*Any doubt *')
 
     reply = await message.client.sendMessage(message.jid,UPLOAD_ING , MessageType.text, { quoted: message.data });
     
-    if (type === 'image') return await message.sendMessage(data, MessageType.image, { caption: "*Codded by MAALUTTY_V3*", quoted: message.data })
+    if (type === 'image') return await message.sendMessage(data, MessageType.image, { caption: "*⁨ɪͥᴛͭsᷤ ᴍᷟᴇͤ ī.am 𝜡𝛯ᗪ⁴⁰⁴⁩*", quoted: message.data })
     
-    if (type === 'video') return await message.sendMessage(data, MessageType.video, { caption: "*Codded by MAALUTTY_V3*", quoted: message.data })
+    if (type === 'video') return await message.sendMessage(data, MessageType.video, { caption: "*⁨ɪͥᴛͭsᷤ ᴍᷟᴇͤ ī.am 𝜡𝛯ᗪ⁴⁰⁴⁩*", quoted: message.data })
     
 });
     
